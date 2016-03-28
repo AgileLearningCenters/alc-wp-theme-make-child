c327c90f3a657f4ba1db01663a8b52dd098325af
Drew Hornbein <dhornbein@gmail.com>
2016-03-27 14:48:24 -1000

    seperated sticky nav js

--- js/child.js
+++ js/child.js
@@ -3,30 +3,8 @@
  *
  * @since 1.0.0
  */
-/* global jQuery, ttfmakeFitVids */
+/* global jQuery */
 (function($) {
-
-	var headerHeight = parseInt($('#site-header').outerHeight());
-	var headerMargin = parseInt($('#site-header').css('marginBottom'));
-	if ($(window).width() > 800){
-		$('#site-content').css('paddingTop',headerHeight + headerMargin);
-	}
-
-  var shrinkHeader = 60;
-  $(window).scroll(function() {
-    var scroll = getCurrentScroll();
-      if ( scroll >= shrinkHeader ) {
-           $('.site-header').addClass('shrink');
-           console.log('shrink');
-        }
-        else {
-            $('.site-header').removeClass('shrink');
-            console.log('unshrink');
-        }
-  });
-  function getCurrentScroll() {
-    return window.pageYOffset || document.documentElement.scrollTop;
-  }
 
   // After arrow color functionality, see .after-arrow in style.css
 
