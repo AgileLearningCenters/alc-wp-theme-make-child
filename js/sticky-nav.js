6aa3c854a9142c34acaa07d44e88c9ffd561ded7
Drew Hornbein <dhornbein@gmail.com>
2016-03-28 00:11:57 -1000

    add existing padding into content height and mesure on element load

--- js/sticky-nav.js
+++ js/sticky-nav.js
@@ -6,11 +6,16 @@
 /* global jQuery */
 (function($) {
 
-  var headerHeight = parseInt($('#site-header').outerHeight());
-  var headerMargin = parseInt($('#site-header').css('marginBottom'));
-  if ($(window).width() > 800){
-    $('#site-content').css('paddingTop',headerHeight + headerMargin);
-  }
+  $('#site-header').ready(function() {
+    
+    var headerHeight = parseInt($('#site-header').outerHeight());
+    var headerMargin = parseInt($('#site-header').css('marginBottom'));
+    var contentPaddingTop = parseInt($('#site-content').css('paddingTop'));
+
+    if ($(window).width() > 800){
+      $('#site-content').css('paddingTop',headerHeight + headerMargin + contentPaddingTop);
+    }
+  });
 
   var shrinkHeader = 60;
   $(window).scroll(function() {
